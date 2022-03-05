2904c4b419309fe7021a3544917bf1faf64d8de2
Bar Karta <69091505+BarKarta@users.noreply.github.com>
2022-03-05 15:01:54 +0200

    Combine Zones
    A script which combine the zones to one data

---
+++ data_prep/combine_data.py
@@ -0,0 +1,17 @@
+import glob
+import pandas as pd
+
+# Get the Manhattan zones LocationID
+nyc_zones = pd.read_csv(
+    "C:\\Users\\barka\\OneDrive\\Final Project\\nyc_zones.csv")
+manhattan_zones = nyc_zones.loc[nyc_zones['Borough']
+                                == 'Manhattan']
+manhattan_zones_ID = manhattan_zones['LocationID'].tolist()
+li = []
+for zone in manhattan_zones_ID:
+    current_path = f'D:\\Final Project\\Data\\2019\\Manhattan_Data\\{zone}'
+    df = pd.read_csv(current_path+'\\post_data_prep.csv')
+    li.append(df)
+main_df = pd.concat(li, axis=0, ignore_index=True)
+main_df.to_csv(
+    f'D:\\Final Project\\Data\\2019\\Manhattan_Data\\post_data_prep_all_zones.csv', index=False)
