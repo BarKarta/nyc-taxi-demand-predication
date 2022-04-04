78d3bd16bbe0fdb685309d8ff8f05adaf1d5bd66
Bar K <Barkarta95@gmail.com>
2022-04-05 02:30:44 +0300

    created a form for the model input

---
+++ demo/client/src/Input_Row.js
@@ -0,0 +1,36 @@
+const Input_Row = () => {
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log("hello");
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <label for="zone">zone:</label>
+        <input type="text" id="zoneID" />
+        <br />
+        <label for="Time">Time:</label>
+        <input type="text" id="timeID" />
+        <br />
+        <label for="zone">zone:</label>
+        <input type="text" id="zoneID" />
+        <br />
+        <label for="zone">zone:</label>
+        <input type="text" id="zoneID" />
+        <br />
+        <label for="zone">zone:</label>
+        <input type="text" id="zoneID" />
+        <br />
+        <label for="zone">zone:</label>
+        <input type="text" id="zoneID" />
+        <br />
+        <label for="zone">zone:</label>
+        <input type="text" id="zoneID" />
+        <br />
+        <input type="submit" value="Submit" />
+      </form>
+    </div>
+  );
+};
+export default Input_Row;
