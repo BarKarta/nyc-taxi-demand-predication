84a43d40ee773b9b48562458a0735378abcb5388
Bar K <Barkarta95@gmail.com>
2022-04-05 15:59:56 +0300

    fix the server side

--- demo/server/server.py
+++ demo/server/server.py
@@ -1,18 +1,23 @@
 from flask import Flask
+from flask import jsonify
+from flask_cors import CORS
 
 app = Flask(__name__)
-
-
-@app.route('/')
-def index():
-    return [{
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
+data = [{
         'name': "Chelsea",
         'values': [
             {'label': "Population", 'val': 20000},
             {'label': "Restaurants", 'val': "690"},
         ],
         'color': "#E31A1C",
-    }]
+        }]
+
+
+@ app.route('/books')
+def index():
+    return jsonify(data)
 
 
 if __name__ == '__main__':
