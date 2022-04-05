3caed481c281bed3f5d36a10284a3096a1c1765e
Bar K <Barkarta95@gmail.com>
2022-04-05 14:17:03 +0300

    add the output of the request

--- demo/server/server.py
+++ demo/server/server.py
@@ -5,7 +5,14 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Hello World'
+    return [{
+        'name': "Chelsea",
+        'values': [
+            {'label': "Population", 'val': 20000},
+            {'label': "Restaurants", 'val': "690"},
+        ],
+        'color': "#E31A1C",
+    }]
 
 
 if __name__ == '__main__':
