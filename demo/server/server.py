2cd51a366c5074fffc3391457ac0df6d4e4b1539
Bar K <Barkarta95@gmail.com>
2022-04-12 00:01:00 +0300

    Create a getDayweek function

--- demo/server/server.py
+++ demo/server/server.py
@@ -3,6 +3,8 @@ from flask import Flask
 from flask import jsonify
 from flask_cors import CORS
 from flask import request
+from datetime import datetime
+import calendar
 
 app = Flask(__name__)
 cors = CORS(app)
@@ -27,8 +29,8 @@ def getTimeBinned(time):
 
 
 def getWeekDay(date):
-    # TODO MAKE FUNCTION
-    return 1
+    my_date = datetime.strptime(date, "%d/%m/%Y")
+    return calendar.day_name[my_date.weekday()]
 
 
 def getZoneName(zone):
@@ -36,26 +38,21 @@ def getZoneName(zone):
     return 1
 
 
-@ app.route('/books')
-def index():
-    return jsonify(data)
-
-
 @app.route('/calc', methods=['POST'])
 def get_input():
     zone = request.json['zone']
     date = request.json['date']
     time = request.json['time']
-    # TODO Add the API and get the weather information needed, based on the data we are getting from the user
-    temp_man, temp_min, hdd, cdd, precipitation, new_snow, snow_depth = getWeather(
-        date, time)
-    # TODO Convert the Time to a interval
-    time_binned = getTimeBinned(time)
+    # # TODO Add the API and get the weather information needed, based on the data we are getting from the user
+    # temp_man, temp_min, hdd, cdd, precipitation, new_snow, snow_depth = getWeather(
+    #     date, time)
+    # # TODO Convert the Time to a interval
+    # time_binned = getTimeBinned(time)
     # TODO Convert the Date to a WeekDay e.g 1/5/22 -> friday
     weekday = getWeekDay(date)
-    # TODO Convert the Zone number to a zone name based on the map the user is look at ( The Map we use in the REACT part).
-    zone_name = getZoneName(zone)
-    return jsonify(f'Zone : {zone}, Data : {date}, Time : {time}')
+    # # TODO Convert the Zone number to a zone name based on the map the user is look at ( The Map we use in the REACT part).
+    # zone_name = getZoneName(zone)
+    return jsonify(f'Zone : {zone}, Date : {date}, Time : {time},WeekDay: {weekday}')
 
 
 if __name__ == '__main__':
